680d1b4cc98feaf9edbd23702988bdc644b99ecd
rahhulramc <ramcrahul@gmail.com>
2018-02-25 23:31:50 +0530

    Add files via upload

---
+++ getsize.py
@@ -0,0 +1,25 @@
+
+import os
+from os.path import join, getsize
+i=0;
+arr=[("?",-1.0)]
+
+def myfn(a):
+	return  a[1];
+def gts(gpath):
+	for root, dirs, files in os.walk(gpath):
+		for name in files:
+			try:
+				temp = getsize(join(root,name))
+			except OSError:
+				continue
+			path=join(root,name)
+			arr.append((path,temp))
+			arr.sort(key=myfn)
+			if(len(arr)>11):
+				arr.remove(arr[1])
+	arr.remove(arr[0])
+	return arr
+	#for i in arr:
+	#	print i[0],float(i[1])/float(1000*1000)	
+	
